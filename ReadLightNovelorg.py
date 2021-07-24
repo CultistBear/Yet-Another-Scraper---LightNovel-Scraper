01447067399175e65548b7692d985913780fdb52
PopCandy <79724656+CultistBear@users.noreply.github.com>
2021-07-24 05:54:09 +0530

    Add files via upload

---
+++ ReadLightNovelorg.py
@@ -0,0 +1,53 @@
+from bs4 import BeautifulSoup
+import cloudscraper
+import os
+import errno
+from __main__ import NovelLinks,NovelTitles,ChapterLinks
+
+os.system("color")
+
+Scraper = cloudscraper.create_scraper()
+
+def Search(Series_Name):
+  headers = {
+      "accept": "*/*",
+      "x-requested-with": "XMLHttpRequest",
+      "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4549.3 Safari/537.36",
+      "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+      "origin": "https://www.readlightnovel.org",
+      "referer": "https://www.readlightnovel.org/",
+  }
+
+  data = {
+    "q": Series_Name
+  }
+
+  response=Scraper.post("https://www.readlightnovel.org/search/autocomplete", headers=headers, data=data)
+  Soup=BeautifulSoup(response.content, "html.parser").find_all("li")
+  for i in Soup:
+    NovelLinks.append(i.a["href"])
+    NovelTitles.append(i.find("span",class_="title").string)
+
+def ChapterRange(ChosenNovelLink):
+  response=Scraper.get(ChosenNovelLink)
+  Soup=BeautifulSoup(response.content,"html.parser").find("div",id="accordion",class_="panel-group")
+  for i in Soup.find_all("ul",class_="chapter-chs"):
+    for j in i.find_all("a",href=True,string=True):
+      ChapterLinks.append(j["href"])
+
+def Download(ToDownloadChapterLink,FinalPath):
+  try:
+    ChapterContent=[]
+    response=""
+    response=Scraper.get(ToDownloadChapterLink)
+    Soup=BeautifulSoup(response.content,"html.parser")
+    for i in Soup.find("div",class_="hidden",id="chapterhidden").find_all("p"):
+      ChapterContent.append(i.text.strip().replace("\n", "")+"\n\n")
+    ChapterTitle=str(Soup.find("div",class_="block-title").a.string+Soup.find("div",class_="block-title").a.next_sibling)
+    CompleteName = os.path.join(FinalPath,ChapterTitle+".txt" )
+    f=open(CompleteName, "w+", encoding="utf-8")
+    for i in ChapterContent:
+        f.write(i)
+    print("\033[0;36m",ChapterTitle," Completed!\033[0;37m")
+  except FileNotFoundError:
+    pass
---
+++ main.py
@@ -0,0 +1,196 @@
+import os
+import time
+from pathlib import Path
+
+os.system("color")
+
+#global variables:
+#DownloadPath
+#LightNovelName
+#ChoosenNovel
+#Choice
+ChapterLinks=[]
+NovelLinks=[]
+NovelTitles=[]
+
+#Edit the Data Below to support your custom source
+
+import ReadLightNovelorg
+
+Sources=["https://www.readlightnovel.org/"]
+
+SourcesSearchFunction={
+    1:ReadLightNovelorg.Search
+    }
+
+SourcesChapterLinksFunction={
+    1:ReadLightNovelorg.ChapterRange
+}
+
+SourcesDownloadFunction={
+    1:ReadLightNovelorg.Download
+}
+
+#End of variables needed to be able to edited to support custom sources.
+
+#Function to Choose the source to scrape the light novels from 
+def SourceChoosing():
+    try:
+        for i in Sources:
+            print("[",Sources.index(i)+1,"]",i,"\n")
+        global Choice
+        Choice=int(input("\033[0;32mChoose a source for downloading Light Novel:\033[0;37m "))
+    except:
+        print("\033[0;31mInvalid Input. Try Again.\033[0;37m")
+        time.sleep(2)
+        print("\n"*3)
+        SourceChoosing()
+    else:
+        if 0<Choice<=len(Sources):
+            SourcesSearchFunction[Choice](LightNovelName)
+            SeriesListing()
+        else:
+            print("\033[0;31mInput outside the range. Try again.\033[0;37m")
+            time.sleep(2)
+            print("\n"*3)
+            SourceChoosing()
+
+#Function that just lists all the light novels found using search string
+def SeriesListing():
+    if len(NovelTitles)==0:
+        print("\033[0;31mThere were no results. Please search again\033[0;37m")
+        time.sleep(2)
+        print("\n"*3)
+        Start()
+    else:
+        print()
+        for i in NovelTitles:
+            print("[",NovelTitles.index(i)+1,"]",i,"\n")
+        ChoosingLightNovelIndex()
+
+#Function to choose one of the light novels to download after search
+def ChoosingLightNovelIndex():
+    try:
+        ChoiceNovel=int(input("\033[0;32mChoose the index of the Light Novel to download: \033[0;37m"))
+    except:
+        print("\033[0;31mInvalid Input. Try Again.\033[0;37m")
+        time.sleep(2)
+        print("\n"*3)    
+        SeriesListing()
+    else:
+        if 0<ChoiceNovel<=len(NovelLinks):
+            global ChoosenNovel
+            ChoosenNovel=NovelTitles[ChoiceNovel-1]
+            SourcesChapterLinksFunction[Choice](NovelLinks[ChoiceNovel-1])
+            StartingChaptersIndex()
+        else:
+            print("\033[0;31mInput outside the range. Try again.\033[0;37m")
+            time.sleep(2)
+            print("\n"*3)
+            SeriesListing()
+
+#Function for choosing the chapter range to download
+def StartingChaptersIndex():
+    try:
+        print("\033[0;32m\nTotal Chapters: \033[0;37m",len(ChapterLinks))
+        ChapterStartingIndex=int(input("\033[0;32mEnter Starting index of chapter(s) to download (1 - %s): \033[0;37m" %(len(ChapterLinks))))
+    except:
+            print("\033[0;31mInvalid Input. Try again.\033[0;37m")
+            time.sleep(2)
+            print("\n"*3)
+            StartingChaptersIndex()
+    else:
+        if 1<=ChapterStartingIndex<=len(ChapterLinks):
+            EndingChaptersIndex(ChapterStartingIndex)
+        else:
+            print("\033[0;31mInput outside the range. Try again.\033[0;37m")
+            time.sleep(2)
+            print("\n"*3)
+            StartingChaptersIndex()
+
+def EndingChaptersIndex(ChapterStartingIndex):
+    try:
+        ChapterEndingIndex=int(input("\033[0;32mEnter Ending index of chapter(s) to download (%s - %s): \033[0;37m" %(ChapterStartingIndex,len(ChapterLinks))))
+    except:
+        print("\033[0;31mInvalid Input. Try again.\033[0;37m")
+        time.sleep(2)
+        print("\n"*3)
+        StartingChaptersIndex()
+    else:
+        if ChapterStartingIndex<=ChapterEndingIndex<=len(ChapterLinks):
+            ChoosingSaveLocation(ChapterStartingIndex,ChapterEndingIndex)
+        else:
+            print("\033[0;31mInput outside the range. Try again.\033[0;37m")
+            time.sleep(2)
+            print("\n"*3)
+            StartingChaptersIndex()
+
+#Function to ask the user for save location of light novels and check the validity of path
+def ChoosingSaveLocation(Beginning,Ending):
+    try:
+        global DownloadPath
+        DownloadPath = str(Path.home() / "Downloads")
+        ChoiceLocation=str(input('''\033[0;32m\nThe default save location for downloads is \033[0;37m%s\033[0;32m 
+Keep in mind that a folder will be created at this location, where the light novel files will be downloaded.
+If you wish to continue with this folder, enter \033[0;37mY\033[0;32m, otherwise enter \033[0;37mN\033[0;32m: \033[0;37m''' %DownloadPath))
+    except:
+            print("\033[0;31mInvalid Input. Try again.\033[0;37m")
+            time.sleep(2)
+            print("\n"*3) 
+            ChoosingSaveLocation(Beginning,Ending) 
+    else:
+        if ChoiceLocation.lower()=="y":
+            if os.path.isdir(DownloadPath)==True:
+                DownloadingFunction(Beginning,Ending,DownloadPath)
+            else:
+                print("\033[0;31mThere were some problems reaching the location of the path. Input another Location.\033[0;37m")
+                time.sleep(2)
+                print("\n"*3) 
+                ChoosingSaveLocation(Beginning,Ending)
+        elif ChoiceLocation.lower()=="n":
+            DownloadPath=input("\033[0;32m\nEnter the Desired Location for Downloads: \033[0;37m")
+            if os.path.isdir(DownloadPath)==True:
+                DownloadingFunction(Beginning,Ending,DownloadPath)
+            else:
+                print("\033[0;31mLocation does not exist or is a file. Input a valid directory.\033[0;37m")
+                time.sleep(2)
+                print("\n"*3) 
+                ChoosingSaveLocation(Beginning,Ending)
+        else:
+            print("\033[0;31mInvalid Input. Try again.\033[0;37m")
+            time.sleep(2)
+            print("\n"*3) 
+            ChoosingSaveLocation(Beginning,Ending)   
+
+#Function Makes a folder at the download location to store the downloaded light novels and checks for write permissions and finally starts the download 
+def DownloadingFunction(Beginning,Ending,Location):
+    FinalPath=os.path.join(Location, ChoosenNovel)
+    PermError=0
+    try:
+        os.mkdir(FinalPath)
+        print("\033[0;32m\nFolder created with path %s.\033[0;37m\n\n" %FinalPath) 
+    except FileExistsError:
+        print("\033[0;32m\nFolder Exists at %s.\033[0;37m\n\n" %FinalPath)
+        pass
+    except PermissionError:
+        print("\033[0;31mInsufficient Permissions to make a folder in that location. Please enter a new path\033[0;37m")
+        time.sleep(2)
+        print("\n"*3)
+        PermError=1
+        pass
+    finally:
+        if PermError==1:
+            ChoosingSaveLocation(Beginning,Ending)
+        else:
+            for i in range(Beginning-1,Ending):
+                SourcesDownloadFunction[Choice](ChapterLinks[i],FinalPath)
+            raise SystemExit("\033[0;32m\nAll Files Downloaded Successfully\033[0;37m")
+
+#Function Starts the Program
+def Start():
+    global LightNovelName
+    LightNovelName=input("\033[0;32mEnter The Name of Light Novel you wish to download:\033[0;37m")
+    print()
+    SourceChoosing()
+
+Start()
